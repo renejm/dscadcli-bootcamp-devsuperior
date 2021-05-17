bbbd0b8b6164e136a76daeb55560816589581c9c
Rene J. Martins <rene.j.martins@gmail.com>
2021-05-17 14:43:13 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscadcli/DscadcliApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscadcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscadcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscadcliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscadcli/DscadcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscadcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscadcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
